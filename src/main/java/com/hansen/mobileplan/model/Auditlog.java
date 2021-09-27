e31a0682de857ef3fe0901a98811b67a5636f1e3
pradyroy <id.pradyroy@gmail.com>
2021-09-27 22:44:48 +0530

    renamed Main.html to index.html

---
+++ src/main/java/com/hansen/mobileplan/model/Auditlog.java
@@ -0,0 +1,45 @@
+package com.hansen.mobileplan.model;
+
+import java.util.Date;
+
+//It's just a POJO not entity class
+public class Auditlog {
+	
+	private Long id;
+	private String operationType;
+	private String entityJson;
+	private Date modificationDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOperationType() {
+		return operationType;
+	}
+
+	public void setOperationType(String operationType) {
+		this.operationType = operationType;
+	}
+
+	public String getEntityJson() {
+		return entityJson;
+	}
+
+	public void setEntityJson(String entityJson) {
+		this.entityJson = entityJson;
+	}
+
+	public Date getModificationDate() {
+		return modificationDate;
+	}
+
+	public void setModificationDate(Date modificationDate) {
+		this.modificationDate = modificationDate;
+	}
+
+}
